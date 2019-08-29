61556c8562ab4f555533711c60d86d90c298ea45
Chris Junker <elevatemedfit@gmail.com>
2019-08-29 17:47:54 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,6 +11,6 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    artists.genres.size#return the number of genres associated with the artist
+    Artist.first#return the number of genres associated with the artist
   end
 end
