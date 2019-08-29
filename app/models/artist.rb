120ad1cfcfa7acd13628eda300a2cb6b24b266af
Chris Junker <elevatemedfit@gmail.com>
2019-08-29 17:41:17 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Artist.genre.first#return the genre of the artist's first saved song
+    prince.genres.first#return the genre of the artist's first saved song
   end
 
   def song_count
